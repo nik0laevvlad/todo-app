5d95067e1127d9020d32328baa0ed33f95285146
Vladislav Nikolaev <chelsea.vlad13@gmail.com>
2022-02-08 23:27:30 +0300

    imported yarn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div>
+    <div className="d-grid gap-2">
+      <Button variant="primary" size="lg">
+        Block level button
+      </Button>
+      <Button variant="success" size="lg">
+        Block level button
+      </Button>
     </div>
   );
 }
