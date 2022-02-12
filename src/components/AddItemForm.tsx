dd1b177babaaa616cc6e56390adc3ea91964852f
Vladislav Nikolaev <chelsea.vlad13@gmail.com>
2022-02-12 22:56:02 +0300

    splitted to components

---
+++ src/components/AddItemForm.tsx
@@ -0,0 +1,27 @@
+import { Button, Col, FormControl, Row } from "react-bootstrap";
+import React, { ChangeEvent } from "react";
+
+interface AddItem {
+  item: string;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  addTodo: () => void;
+}
+
+export const AddItemForm = ({item, handleChange, addTodo}: AddItem) => {
+  const pressEnter = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      addTodo();
+    }
+  }
+
+  return (
+    <Row className="mt-5 justify-content-md-center">
+      <Col md="auto">
+        <FormControl onKeyPress={pressEnter} name="input" placeholder="ToDo" value={item} onChange={handleChange}/>
+      </Col>
+      <Col md="auto">
+        <Button onClick={addTodo}>Submit</Button>
+      </Col>
+    </Row>
+  )
+}
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export * from './AddItemForm'
+export * from './ListItem'
+export * from './TodoList'
